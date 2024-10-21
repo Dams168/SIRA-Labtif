7ea76ad8d140591f99512e1f1796c2c74d3f8875
Dams168 <adamrohman2003@gmail.com>
2024-10-21 22:55:21 +0700

    update: remove forgot password temporary

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,12 +22,12 @@
                     required="">
                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
             </div>
-            <div class="flex items-center justify-end">
+            {{-- <div class="flex items-center justify-end">
                 @if (Route::has('password.request'))
                     <a href="{{ route('password.request') }}"
                         class="text-sm font-medium text-blue-500 hover:underline">Forgot password?</a>
                 @endif
-            </div>
+            </div> --}}
             <button type="submit"
                 class="w-full text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign
                 in</button>
