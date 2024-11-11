465a6859ee2b49869091a5d9e4618c8892bd830d
Dams168 <adamrohman2003@gmail.com>
2024-11-11 23:58:56 +0700

    add: filtering status

--- resources/views/admin/file/index.blade.php
+++ resources/views/admin/file/index.blade.php
@@ -6,45 +6,66 @@
     <section class="bg-gray-900">
         <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-12">
             <h2 class="text-4xl font-medium text-gray-300 pb-6">Kelola Berkas</h2>
-            <div class="relative overflow-x-auto">
-                <div class="min-w-full inline-block align-middle">
-                    <table class="w-full text-sm text-left text-gray-400 bg-gray-900">
-                        <thead class="text-xs uppercase bg-gray-700 text-gray-400 text-center">
-                            <tr>
-                                <th scope="col" class="px-3 py-3">No</th>
-                                <th scope="col" class="px-3 py-3">Nama</th>
-                                <th scope="col" class="px-3 py-3">NPM</th>
-                                <th scope="col" class="px-3 py-3">Kelas</th>
-                                <th scope="col" class="px-3 py-3">tgl Daftar</th>
-                                <th scope="col" class="px-3 py-3">Matkul Minat</th>
-                                <th scope="col" class="px-3 py-3">Status Pendaftaran</th>
-                                <th scope="col" class="px-3 py-3">Aksi</th>
+            <div class="flex items-center justify-end mb-4">
+                <label for="filterStatus" class="text-gray-300 text-lg mr-4">Filter Status:</label>
+                <select id="filterStatus" onchange="filterStatus()"
+                    class="block w-1/6 p-2 text-gray-900 bg-white rounded-md shadow-sm  border border-gray-400">
+                    <option value="">Semua</option>
+                    <option value="Menunggu">Menunggu</option>
+                    <option value="Diterima">Diterima</option>
+                    <option value="Ditolak">Ditolak</option>
+                </select>
+            </div>
+            <div class="relative overflow-x-auto shadow-md rounded-lg">
+                <table class="w-full text-sm text-left text-gray-400 bg-gray-900">
+                    <thead class="text-xs uppercase bg-gray-700 text-gray-400 text-center">
+                        <tr>
+                            <th scope="col" class="px-3 py-3">No</th>
+                            <th scope="col" class="px-3 py-3">Nama</th>
+                            <th scope="col" class="px-3 py-3">NPM</th>
+                            <th scope="col" class="px-3 py-3">Kelas</th>
+                            <th scope="col" class="px-3 py-3">tgl Daftar</th>
+                            <th scope="col" class="px-3 py-3">Matkul Minat</th>
+                            <th scope="col" class="px-3 py-3">Status Pendaftaran</th>
+                            <th scope="col" class="px-3 py-3">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($registrations as $key => $registration)
+                            <tr class="text-center bg-gray-800 border-b border-gray-700">
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $registration->name }}</td>
+                                <td>{{ $registration->npm }}</td>
+                                <td>{{ $registration->class }} - {{ $registration->period }}</td>
+                                <td>{{ $registration->regDate }}</td>
+                                <td>{{ $registration->course->name }}</td>
+                                <td>{{ $registration->status }}</td>
+                                <td>
+                                    <x-primary-button tag="a" href="{{ route('show.verify', $registration->id) }}">
+                                        Verifikasi
+                                    </x-primary-button>
+                                </td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($registrations as $key => $registration)
-                                {{-- @if ($registration->status === 'Menunggu') --}}
-                                <tr class="text-center bg-gray-800 border-b border-gray-700">
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{ $registration->name }}</td>
-                                    <td>{{ $registration->npm }}</td>
-                                    <td>{{ $registration->class }} - {{ $registration->period }}</td>
-                                    <td>{{ $registration->regDate }}</td>
-                                    <td>{{ $registration->course->name }}</td>
-                                    <td>{{ $registration->status }}</td>
-                                    <td>
-                                        <x-primary-button tag="a"
-                                            href="{{ route('show.verify', $registration->id) }}">
-                                            Verifikasi
-                                        </x-primary-button>
-                                    </td>
-                                </tr>
-                                {{-- @endif --}}
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </section>
+
+    <script>
+        function filterStatus() {
+            var filterStatus = document.getElementById('filterStatus').value;
+            var rows = document.getElementsByTagName('tr');
+            for (var i = 1; i < rows.length; i++) {
+                var row = rows[i];
+                var status = row.cells[6].innerText;
+                if (filterStatus === '' || filterStatus === status) {
+                    row.style.display = '';
+                } else {
+                    row.style.display = 'none';
+                }
+            }
+        }
+    </script>
 @endsection
--- resources/views/admin/score/index.blade.php
+++ resources/views/admin/score/index.blade.php
@@ -7,6 +7,25 @@
         <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-12">
             <h2 class="text-4xl font-medium text-gray-300 pb-6">Kelola Nilai</h2>
             <div class="relative overflow-x-auto">
+                <div class="flex items-center justify-end mb-4">
+                    <label for="filterPeriod" class="text-gray-300 text-lg mr-4">Filter Angkatan:</label>
+                    <select id="filterPeriod" onchange="filterPeriod()"
+                        class="block w-1/6 p-2 text-gray-900 bg-white rounded-md shadow-sm  border border-gray-400">
+                        <option value="">Semua</option>
+                        @for ($i = date('Y'); $i >= 2021; $i--)
+                            <option value="{{ $i }}">{{ $i }}</option>
+                        @endfor
+                    </select>
+
+                    <label for="filterStatus" class="text-gray-300 text-lg mr-4">Filter Status:</label>
+                    <select id="filterStatus" onchange="filterStatus()"
+                        class="block w-1/6 p-2 text-gray-900 bg-white rounded-md shadow-sm  border border-gray-400">
+                        <option value="">Semua</option>
+                        <option value="Menunggu">Menunggu</option>
+                        <option value="Diterima">Diterima</option>
+                        <option value="Ditolak">Ditolak</option>
+                    </select>
+                </div>
                 <div class="min-w-full inline-block align-middle">
                     <table class="w-full text-sm text-left text-gray-400 bg-gray-900">
                         <thead class="text-xs uppercase bg-gray-700 text-gray-400 text-center">
@@ -48,4 +67,34 @@
             </div>
         </div>
     </section>
+
+    <script>
+        function filterStatus() {
+            var filterStatus = document.getElementById('filterStatus').value;
+            var rows = document.getElementsByTagName('tr');
+            for (var i = 1; i < rows.length; i++) {
+                var row = rows[i];
+                var status = row.cells[8].innerText;
+                if (filterStatus === '' || status === filterStatus) {
+                    row.style.display = '';
+                } else {
+                    row.style.display = 'none';
+                }
+            }
+        }
+
+        function filterPeriod() {
+            var filterPeriod = document.getElementById('filterPeriod').value;
+            var rows = document.getElementsByTagName('tr');
+            for (var i = 1; i < rows.length; i++) {
+                var row = rows[i];
+                var period = row.cells[2].innerText.split(' - ')[1];
+                if (filterPeriod === '' || period === filterPeriod) {
+                    row.style.display = '';
+                } else {
+                    row.style.display = 'none';
+                }
+            }
+        }
+    </script>
 @endsection
