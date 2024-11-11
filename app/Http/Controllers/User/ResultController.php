2064cbc11cbdc41a77b60ea77548402816857d57
Dams168 <adamrohman2003@gmail.com>
2024-11-12 00:10:24 +0700

    fix

--- app/Http/Controllers/User/ResultController.php
+++ app/Http/Controllers/User/ResultController.php
@@ -12,23 +12,21 @@ class ResultController extends Controller
     public function accepted()
     {
         $result = result::where('result', 'Diterima')->get()->first();
-        $registration = $result ? $result->test->registration : null;
 
         $registrations = Registration::whereHas('test.result', function ($query) {
             $query->where('result', 'Diterima');
         })->get();
-        return view('users.result.accepted', compact('result', 'registration', 'registrations'));
+        return view('users.result.accepted', compact('result', 'registrations'));
     }
 
     public function rejected()
     {
         $result = result::where('result', 'Ditolak')->get()->first();
 
-        $registration = $result ? $result->test->registration : null;
         $registrations = Registration::whereHas('test.result', function ($query) {
             $query->where('result', 'Diterima');
         })->get();
 
-        return view('users.result.rejected', compact('result', 'registration', 'registrations'));
+        return view('users.result.rejected', compact('result',  'registrations'));
     }
 }
